0ae07ab40afb15a0de2bbd1bfb7d862072421cb5
shubham <shubham.mittal@gmail.com>
2012-02-16 00:59:02 -0800

    fixing the message_for_sending method.. in case the payload exceeds 256 bytes, the string creation was causing an exception even before the error checking could happen

--- lib/apn_on_rails/app/models/apn/notification.rb
+++ lib/apn_on_rails/app/models/apn/notification.rb
@@ -80,9 +80,8 @@ class APN::Notification < APN::Base
   # Creates the binary message needed to send to Apple.
   def message_for_sending
     json = self.to_apple_json
-    message = "\0\0 #{self.device.to_hexa}\0#{json.length.chr}#{json}"
-    raise APN::Errors::ExceededMessageSizeError.new(message) if message.size.to_i > 256
-    message
+    raise APN::Errors::ExceededMessageSizeError.new(json) if json.length.to_i > 255
+    "\0\0 #{self.device.to_hexa}\0#{json.length.chr}#{json}"
   end
   
   def self.send_notifications
